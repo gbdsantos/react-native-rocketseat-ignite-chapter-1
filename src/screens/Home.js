bdce89c03ebc96300654868e95eb7a7a74890330
Guilherme Bezerra <gbsantos.it@gmail.com>
2021-10-28 12:14:00 -0300

    :pencil: update(mobile): use stylesheet

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome Guilherme</Text>
-      <TextInput style={styles.input} />
+      <Text style={styles.title}>
+        Welcome Guilherme
+      </Text>
+
+      <TextInput
+        placeholder="New Skill"
+        placeholderTextColor="#555"
+        style={styles.input}
+      />
+
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={styles.button}
+      >
+        <Text style={styles.buttons}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 20 }]}>
+        My Skills
+      </Text>
     </View>
   )
 }
@@ -30,6 +55,20 @@ const styles = StyleSheet.create({
     color: '#FFF',
     fontSize: 18,
     marginTop: 30,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+  },
+
+  button: {
+    backgroundColor: '#A370F7',
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
     padding: 15,
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: 'bold',
   }
 });
